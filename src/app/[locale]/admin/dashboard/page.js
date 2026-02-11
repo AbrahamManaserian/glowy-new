ceef3444c4e765f59bbc5b12124689397fad3d3d
AbrahamManaserian <abraham020788@gmail.com>
2026-02-11 11:32:57 +0400

    ...

--- src/app/[locale]/admin/dashboard/page.js
+++ src/app/[locale]/admin/dashboard/page.js
@@ -36,7 +36,7 @@ export default function Dashboard() {
             Deposits
           </Typography>
           <Typography component="p" variant="h4">
-            $3,024.00
+            $3,024.000
           </Typography>
           <Typography color="text.secondary" sx={{ flex: 1 }}>
             on 15 March, 2019
